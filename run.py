ebe58c9443efd8d883ab6c37046f69bb703a9d75
Vanessa <vanessa.omondi@student.moringaschool.com>
2026-02-13 02:49:11 +0300

    Load backend env variables

--- run.py
+++ run.py
@@ -1,6 +1,18 @@
 from app import create_app
+from dotenv import load_dotenv
+import os
 
+# Load .env
+load_dotenv()
+
+# Read env variables
+SECRET_KEY = os.getenv("SECRET_KEY")
+DEBUG = os.getenv("DEBUG") == "True"
+PORT = int(os.getenv("PORT", 5000))
+
+# Pass secret key to your app
 app = create_app()
+app.config['SECRET_KEY'] = SECRET_KEY  # <-- use it here
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == "_main_":
+    app.run(debug=DEBUG, port=PORT)  # <-- use DEBUG and PORT
